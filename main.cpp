05aee0ab66e1dcc3f4798e3bc4610e5d3aa5dc1f
Alice (ShukhtinaAN) <alice@exampl.com>
2022-04-25 08:46:45 +0300

    git: добавлена печать максимума

--- main.cpp
+++ main.cpp
@@ -10,4 +10,9 @@ int main()
     cout << "A + B = " << a + b << '\n' << "A - B = " << a - b;
     cout << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
+    if (a > b)
+    cout <<"MAX="<< a;
+    else
+       if (a < b)
+       cout << "MAX="  << b;
 }
