8c0143a5fa9adda07d297adff24b85c1e92aff92
Bob (ShukhtinaAN) <bob@example.com>
2022-04-25 08:49:43 +0300

    code: добавлен вывод минимума

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
+<<<<<<< HEAD
     cout << "A + B = " << a + b << '\n' << "A - B = " << a - b;
     cout << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
@@ -15,4 +16,15 @@ int main()
     else
        if (a < b)
        cout << "MAX="  << b;
+=======
+    cout << "A + B = " << a + b << '\n'
+         << "A - B = " << a - b
+         << "A * B = " << a * b << '\n';
+
+    if (a < b)
+         cout << "MIN=" << a;
+         else
+           if (a > b)
+           cout << "MIN=" << b;
+>>>>>>> code: добавлен вывод минимума
 }
